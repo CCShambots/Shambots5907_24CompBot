67cfb7ef727802d4f216711a840ceb22d89a518f
Breadspeed1 <53412269+Breadspeed1@users.noreply.github.com>
2024-01-23 01:33:25 -0500

    new indexing logic

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -4,6 +4,7 @@ import static frc.robot.Constants.Indexer.Settings.*;
 
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.ShamLib.SMF.StateMachine;
+import java.util.concurrent.atomic.AtomicBoolean;
 import org.littletonrobotics.junction.Logger;
 
 public class Indexer extends StateMachine<Indexer.State> {
@@ -23,26 +24,20 @@ public class Indexer extends StateMachine<Indexer.State> {
     registerStateCommand(
         State.EXPECT_RING_BACK,
         new SequentialCommandGroup(
-                new InstantCommand(() -> io.setTargetVelocity(EXPECT_SPEED)),
-                new WaitUntilCommand(() -> inputs.prox1),
-                new InstantCommand(() -> io.setTargetVelocity(INDEX_SPEED)),
-                new WaitUntilCommand(() -> inputs.prox2),
-                new InstantCommand(io::stop))
-            .withTimeout(INDEX_TIMEOUT)
-            .andThen(
-                new ConditionalCommand(
-                    transitionCommand(State.HOLDING_RING),
-                    transitionCommand(State.LOST_RING),
-                    () -> inputs.prox1 && inputs.prox2)));
+            new InstantCommand(() -> io.setTargetVelocity(EXPECT_SPEED)),
+            new WaitUntilCommand(() -> inputs.prox1),
+            transitionCommand(State.INDEXING)));
 
     registerStateCommand(
         State.EXPECT_RING_FRONT,
         new SequentialCommandGroup(
-                new InstantCommand(() -> io.setTargetVelocity(-EXPECT_SPEED)),
-                new WaitUntilCommand(() -> inputs.prox2),
-                new InstantCommand(() -> io.setTargetVelocity(-INDEX_SPEED)),
-                new WaitUntilCommand(() -> inputs.prox1),
-                new InstantCommand(io::stop))
+            new InstantCommand(() -> io.setTargetVelocity(-EXPECT_SPEED)),
+            new WaitUntilCommand(() -> inputs.prox2),
+            transitionCommand(State.INDEXING)));
+
+    registerStateCommand(
+        State.INDEXING,
+        indexCommand()
             .withTimeout(INDEX_TIMEOUT)
             .andThen(
                 new ConditionalCommand(
@@ -73,14 +68,33 @@ public class Indexer extends StateMachine<Indexer.State> {
     addOmniTransition(State.IDLE);
     addOmniTransition(State.CLEANSE);
 
-    addTransition(State.EXPECT_RING_FRONT, State.LOST_RING);
-    addTransition(State.EXPECT_RING_BACK, State.LOST_RING);
-
-    addTransition(State.EXPECT_RING_BACK, State.HOLDING_RING);
-    addTransition(State.EXPECT_RING_FRONT, State.HOLDING_RING);
+    addTransition(State.INDEXING, State.LOST_RING);
+    addTransition(State.INDEXING, State.HOLDING_RING);
 
     addTransition(State.HOLDING_RING, State.FEED_TO_SHOOTER);
     addTransition(State.HOLDING_RING, State.PASS_THROUGH);
+
+    addTransition(State.EXPECT_RING_BACK, State.INDEXING);
+    addTransition(State.EXPECT_RING_FRONT, State.INDEXING);
+  }
+
+  private Command indexCommand() {
+    AtomicBoolean isFinished = new AtomicBoolean(false);
+
+    return new FunctionalCommand(
+        () -> isFinished.set(false),
+        () -> {
+          // either both are set and we have ring or both are unset and we lost ring
+          if (inputs.prox1 == inputs.prox2) isFinished.set(true);
+
+          // if first prox is on but second isnt, run forwards
+          else if (inputs.prox1) io.setTargetVelocity(INDEX_SPEED);
+
+          // otherwise vice versa
+          else io.setTargetVelocity(-INDEX_SPEED);
+        },
+        (interrupted) -> io.stop(),
+        isFinished::get);
   }
 
   @Override
@@ -100,6 +114,7 @@ public class Indexer extends StateMachine<Indexer.State> {
     IDLE,
     EXPECT_RING_FRONT,
     EXPECT_RING_BACK,
+    INDEXING,
     PASS_THROUGH,
     FEED_TO_SHOOTER,
     CLEANSE,
