713b89cf39d09f0a48f29706911d23ab3faa66d2
aiden <aiden@voth.name>
2024-01-31 09:13:16 -0500

    shooter amp stuff

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -77,6 +77,12 @@ public class Shooter extends StateMachine<Shooter.State> {
             arm.transitionCommand(Arm.State.SOFT_E_STOP),
             flywheel.transitionCommand(Flywheel.State.IDLE)));
 
+    registerStateCommand(State.AMP, new ParallelCommandGroup(
+            arm.transitionCommand(Arm.State.AMP),
+            flywheel.transitionCommand(Flywheel.State.AMP),
+            watchReadyCommand()
+    ));
+
     registerStateCommand(
         State.BASE_SHOT,
         new ParallelCommandGroup(
@@ -146,6 +152,7 @@ public class Shooter extends StateMachine<Shooter.State> {
     addOmniTransition(State.TRAP);
     addOmniTransition(State.TRAP_PREP);
     addOmniTransition(State.CHUTE_INTAKE);
+    addOmniTransition(State.AMP);
 
     addTransition(State.SOFT_E_STOP, State.BOTTOM_FLYWHEEL_VOLTAGE_CALC);
     addTransition(State.SOFT_E_STOP, State.FLYWHEEL_VOLTAGE_CALC);
@@ -214,6 +221,7 @@ public class Shooter extends StateMachine<Shooter.State> {
     FLYWHEEL_VOLTAGE_CALC,
     BOTTOM_FLYWHEEL_VOLTAGE_CALC,
     ARM_VOLTAGE_CALC,
+    AMP,
     // flags
     READY
   }
